87823ac7ceda74269e773408224484939a84d1e9
Baris28 <bbekdemir@roc-dev.com>
2017-06-16 10:14:08 +0200

    first commit

---
+++ Kapperszaak.php
@@ -0,0 +1,7 @@
+<?php
+print("De volgende momenten zijn nog beschikbaar:<ul>");
+foreach($kappersagenda as $afspraak => $tijd) {
+    if($afspraak == "") {
+        print("<li>".$tijd."</li>") ;
+    }
+}
